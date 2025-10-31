a521bbd6022df57de7775106f59d8a33f82dcb1b
Dalitso Zulu <133605639+DalitsoZz@users.noreply.github.com>
2025-10-31 14:40:05 +0200

    Update .git-rewrite-msg.sh

--- .git-rewrite-msg.sh
+++ .git-rewrite-msg.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-# Helper script for git filter-branch --commit-filter
-# Replaces the commit message for a specific commit SHA with a neutral message.
 
 TARGET_SHA="d8dd5470ab0bb3032e183e3f07b353df5077f40b"
 NEW_MSG="initial import"
